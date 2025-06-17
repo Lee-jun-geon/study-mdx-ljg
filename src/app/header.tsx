8bc04cdfa73846cbdc406e97c1d448fbfa3606e9
이준건 <jungeon9017@gmail.com>
2025-06-17 19:38:46 +0900

    fix: heder 디자인

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 z-10 bg-white/30 backdrop-blur-xl h-[70px] items-center flex px-4 border-b border-black/10'>
-      <Link href='/' className='text-2xl font-bold'>
-        MDX Blog
-      </Link>
-    </header>
+<header className='sticky top-0 z-10 bg-gradient-to-r from-pink-100 via-white to-blue-100 backdrop-blur-md h-[90px] flex items-center px-6 border-b-2 border-gray-200 shadow-md'>
+  <Link href='/' className='text-3xl font-semibold text-gray-800 hover:text-blue-500 transition duration-300'>
+    LJG Blog
+  </Link>
+</header>
   );
 }
